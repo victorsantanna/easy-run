7e4f5fc3e715518f824ad7d589286584c7d371c5
victorsantanna <victor.esef@hotmail.com>
2024-05-20 11:35:13 -0300

    feat: adiciona as funcionalidades do componente eventos.

---
+++ src/views/EventosView.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Eventos</h1>
+    <div>
+        <ul>
+            <li v-for="evento in eventos" :key="evento.nome">
+                Nome: {{ evento.nome }}<br>
+                Local: {{ evento.Local }}<br>
+                Data: {{ evento.data }}<br>
+                Distâncias: {{ evento.Distancia }}<br> <br>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const eventos = [
+    {nome: 'Night Run', Local:'Recife', data: '25/05/2024', Distancia:'10/5km'},
+    {nome: '19ª Corrida das Pontes do Recife', Local:'Recife', data: '02/06/2024', Distancia:'10/5km'},
+    {nome: 'Music Running 2024 - São João do Mundo', Local:'Caruaru', data: '09/06/2024', Distancia:'10/5km'},
+    {nome: '46ª Corrida Guararapes', Local:'Recife', data: '09/06/2024', Distancia:'10/5km'},
+    {nome: 'Maratona Internacional de Olinda', Local:'Olinda', data: '16/06/2024', Distancia:'42/21/10/5km'},
+    {nome: 'Circuito das Estações 2024 - Inverno', Local:'Recife', data: '07/07/2024', Distancia:'15/10/5km'},
+
+]
+
+</script>
