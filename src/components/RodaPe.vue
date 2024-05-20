3141e6ab058e9c813059f0df11443eaf00852a2d
victorsantanna <victor.esef@hotmail.com>
2024-05-20 11:33:23 -0300

    feat: cria o componente roda pé

---
+++ src/components/RodaPe.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered" >
+            <h4>Easy Run</h4>
+        </div>
+
+    </footer>
+    
+</template>
+
+<style>
+.footer{
+    background-color: #66D1FF;
+    padding: 1rem 1.5rem;
+   
+}
+.footer .content {
+  padding: 1px; 
+}
+.footer h4 {
+  margin: 0; 
+  font-size: 1.25rem; 
+  color:aliceblue;
+  text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.6);
+}
+</style>
