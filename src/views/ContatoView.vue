bf5672aaa8b79ab583a81b2c14519406cb488cd8
victorsantanna <victor.esef@hotmail.com>
2024-05-19 23:50:53 -0300

    feat: adiciona o data binding do formulário.

--- src/views/ContatoView.vue
+++ src/views/ContatoView.vue
@@ -2,15 +2,21 @@
     <div class="columns">
         <div class="column">
             Formulário
-            <input class="input is-info mb-3" type="text" placeholder="Nome"/>
-            <input class="input is-info mb-3" type="text" placeholder="E-mail"/>
-            <textarea class="textarea is-info mb-3" placeholder="Info textarea" rows="10"></textarea>
+            <input class="input is-info mb-3" type="text" placeholder="Nome" v-model="nome"/>
+            <input class="input is-info mb-3" type="text" placeholder="E-mail" v-model="email"/>
+            <textarea class="textarea is-info mb-3" placeholder="Info textarea" rows="10" v-model="mensagem"></textarea>
             <button class="button is-info is-fullwidth ">Enviar</button>
+           
         </div>
         <div class="column">Imagem</div>
     </div>
 </template>
 
-<script>
+<script setup>
+    import { ref } from 'vue'
+    const nome = ref('')
+    const email = ref('')
+    const mensagem = ref('')
+
 
 </script>
