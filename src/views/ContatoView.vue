b4f61c8e1c8d6af41649f965f69dc4ff732a39ff
victorsantanna <victor.esef@hotmail.com>
2024-05-20 15:39:09 -0300

    feat: adiciona class container no componente

--- src/views/ContatoView.vue
+++ src/views/ContatoView.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="columns">
-        <div class="column" id="form">
-            Formulário
-            <input class="input is-info mb-3" type="text" placeholder="Nome" v-model="nome"/>
-            <input class="input is-info mb-3" type="text" placeholder="E-mail" v-model="email"/>
-            <textarea class="textarea is-info mb-3" placeholder="Info textarea" rows="5" v-model="mensagem"></textarea>
-            <button class="button is-info is-fullwidth ">Enviar</button>
-           
+    <div class="container">
+        <div class="columns">
+            <div class="column" id="form">
+                Formulário
+                <input class="input is-info mb-3" type="text" placeholder="Nome" v-model="nome"/>
+                <input class="input is-info mb-3" type="text" placeholder="E-mail" v-model="email"/>
+                <textarea class="textarea is-info mb-3" placeholder="Info textarea" rows="5" v-model="mensagem"></textarea>
+                <button class="button is-info is-fullwidth ">Enviar</button>
+               
+            </div>
+            <div class="column">
+               <img src="/public/img/easyrunemail.png" alt="">
+            </div>
         </div>
-        <div class="column">
-           <img src="/public/img/easyrunemail.png" alt="">
-        </div>
+
     </div>
 </template>
 
@@ -23,6 +26,10 @@
 
 </script>
 
-<style>
+<style scoped>
+.container{
+    width: 93%;
+    margin-top: 15px;
+}
 
 </style>
