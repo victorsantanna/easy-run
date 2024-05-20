8500d9c7bf6a1edf6bf5dfae206677aa22ea0a87
victorsantanna <victor.esef@hotmail.com>
2024-05-19 22:42:02 -0300

    feat: adiciona o sobre nos no View do app.

---
+++ src/views/SobreNosView.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <h3>Sobre Nós</h3>
+    <p>Bem-vindo ao nosso mundo de eventos de corrida. Onde a paixão encontra o profissionalismo e a criatividade encontra a precisão.
+      Aqui na Easy Run estamos dedicados a criar experiências que transcendem o comum.</p>
+    <h3>Nossa Missão</h3>
+    <p>Na Easy Run, nossa missão é transformar o mundo dos eventos de corrida, unindo paixão e profissionalismo para criar experiências inesquecíveis. 
+      Acreditamos que cada evento é uma oportunidade de inspirar e conectar pessoas, celebrando a alegria e a adrenalina das corridas. 
+      Com criatividade e precisão, nos dedicamos a superar expectativas, oferecendo uma plataforma inovadora e acessível que transcende o comum. 
+      Estamos comprometidos em proporcionar momentos únicos para corredores de todos os níveis, promovendo saúde, bem-estar e um espírito de comunidade vibrante.</p>
+    <h3>Nossa Equipe</h3>
+    <p>
+      Na Easy Run, nossa equipe é o coração e a alma de tudo o que fazemos. Composta por profissionais apaixonados e dedicados, 
+      cada membro traz uma combinação única de habilidades e experiências para criar eventos de corrida excepcionais. 
+      Nosso time é formado por especialistas em planejamento de eventos, marketing, logística e atendimento ao cliente, todos unidos por um amor compartilhado pela corrida.
+    </p>
+    <p>
+      Trabalhamos juntos para garantir que cada detalhe seja perfeito, desde a inscrição até a linha de chegada. 
+      Nossa equipe acredita no poder transformador das corridas e se esforça para proporcionar experiências memoráveis 
+      que inspirem e conectem corredores de todas as idades e habilidades. Com um compromisso inabalável com a excelência, 
+      a equipe Easy Run está aqui para apoiar e motivar você em cada passo do caminho.
+    </p>
+
+    <p>
+      Estamos animados para compartilhar histórias inspiradoras e experiências incríveis com todos os nossos corredores e entusiastas.
+      Na Easy Run, acreditamos que cada corrida é uma jornada única, cheia de emoção e conquistas.
+    </p>
+    <p>
+      Saiba mais sobre nós e descubra como podemos tornar a sua próxima corrida uma experiência inesquecível.
+    </p>
+</template>
