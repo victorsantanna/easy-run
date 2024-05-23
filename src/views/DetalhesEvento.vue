96e00d9e1e6b8d105c9153d99e0182ef546050fd
victorsantanna <victor.esef@hotmail.com>
2024-05-23 10:50:29 -0300

    feat: adiciona as interpolações  dos dados de cada card.

--- src/views/DetalhesEvento.vue
+++ src/views/DetalhesEvento.vue
@@ -1,6 +1,70 @@
 <template>
-    <div>
+  <div class="container-principal">
+    <div class="container">
       <h1>Detalhes do Evento</h1>
-      <p>ID do Evento: {{ $route.params.id }}</p>
-    </div>
+      <div v-if="id">
+        
+        <p><strong>Nome:</strong> {{ nome }}</p>
+        <p><strong>Local:</strong> {{ local }}</p>
+        <p><strong>Data:</strong> {{ data }}</p>
+        <p><strong>Distâncias:</strong> {{ distancia }}</p>
+
+      </div>
+
+      <div v-else>
+        <p>Evento não encontrado.</p>
+      </div>
+
+  </div>
+
+  </div>
 </template>
+
+<script setup>
+
+  defineProps({
+  id: Number,
+  nome: String,
+  local: String,
+  data: String,
+  distancia: String
+})
+
+</script>
+
+<style scoped>
+.container-principal{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 600px;
+  height: 68vh;
+  margin: 9px;
+  padding: 20px;
+  border: 1.5px solid #ccc;
+  border-radius: 10px; 
+  box-shadow: 0 5px 10px rgba(0, 0, 0.3, 0.3);
+}
+h1 {
+  margin: 10px;
+  font-family: tahoma, sans-serif;
+  font-weight: bold;
+  font-size: 25px;
+}
+
+p {
+  margin: 6px 0;
+}
+
+strong {
+  font-weight: bold;
+}
+</style>
